fd97f286b6f4d3cec4507b07c612185ed77a8485
Promise Emmanuel Oluwadare <95595405+PromiseGameFi@users.noreply.github.com>
2026-03-03 15:25:33 +0100

    Create setup.ts

---
+++ src/test/setup.ts
@@ -0,0 +1,16 @@
+import '@testing-library/jest-dom/vitest'
+import 'fake-indexeddb/auto'
+
+if (!window.matchMedia) {
+  window.matchMedia = () =>
+    ({
+      matches: false,
+      media: '',
+      onchange: null,
+      addListener: () => {},
+      removeListener: () => {},
+      addEventListener: () => {},
+      removeEventListener: () => {},
+      dispatchEvent: () => false,
+    }) as MediaQueryList
+}
